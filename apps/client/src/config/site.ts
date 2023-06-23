6079fc2191cca7ccfa2469b9e506e4c652ef1432
Rohan Sen <rohansen856@gmail.com>
2023-06-23 23:15:55 +0530

    modified:   apps/client/src/config/site.ts

--- apps/client/src/config/site.ts
+++ apps/client/src/config/site.ts
@@ -1,10 +1,11 @@
 import { SiteConfig } from "@/types"
+import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
   name: "ChessHub",
   description: "A brand new chess application with features never seen before.",
-  url: "http://localhost:3000",
-  ogImage: "http://localhost:3000/og.jpg",
+  url: env.NEXT_PUBLIC_APP_URL as string || "http://localhost:3000",
+  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
   links: {
     twitter: "https://twitter.com/shadcn",
     github: "https://github.com/shadcn/taxonomy",
