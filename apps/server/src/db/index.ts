16354c9eed074f84efddac6675505eebdd323a79
Rohan Sen <rohansen856@gmail.com>
2023-06-24 00:41:27 +0530

    modified:   apps/server/package.json 	modified:   apps/server/src/db/index.ts

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,12 +1,4 @@
-import { Pool } from "pg";
-
-export const db = new Pool({
-  user: process.env.POSTGRES_DB_USER || "postgres",
-  host: process.env.POSTGRES_DB_HOST || "db.nkpkvtpxnvlzsaddhsgm.supabase.co",
-  database: process.env.POSTGRES_DB_DATABASE || "postgres",
-  password: process.env.POSTGRES_DB_PASSWORD || "rohan0subarna",
-  port: 5432,
-});
+export { pgTable as db } from "database";
 
 export const INIT_TABLES = /* sql */ `
     CREATE TABLE IF NOT EXISTS "user" (
